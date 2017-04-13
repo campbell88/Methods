2c446209fe0134518e6bf1e35061103f56538f82
campbell88 <shatharcampbell@gmail.com>
2017-04-13 15:54:47 -0400

    valid integer method and valid y/n method

---
+++ Methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args) //this Project was created specifically for methods created that can be placed in new programs
+        {
+
+        }   
+            
+        public static int GetValidInteger()
+        {
+            int input;
+
+            Console.WriteLine("Please enter an Integer: ");
+
+            while (!int.TryParse(Console.ReadLine(), out input))
+            {
+                Console.WriteLine("The input is not an integer! Please enter an integer! ");
+            }
+
+            return input;
+        }
+
+
+        public static char GetValidChar()
+        {
+            char Letter;
+
+            while (!char.TryParse(Console.ReadLine(), out Letter))
+            {
+                Console.WriteLine("The input is not 'y' or 'n'!");
+            }
+            return Letter;
+        }
+
+    }
+    }
+
